17864d83d7b8b84ee57b4806278ab7a95695ebb5
Adrian Pikios <adrian@evokelabs.com>
2022-06-01 16:11:24 +1000

    Node JS

---
+++ backend/models/http-error.js
@@ -0,0 +1,11 @@
+class HttpError extends Error {
+  constructor(message, status, code) {
+    super(message)
+    this.name = this.constructor.name
+    this.message = message
+    this.status = status
+    this.code = code
+  }
+}
+
+module.exports = HttpError
---
+++ backend/util/location.js
@@ -0,0 +1,5 @@
+function getCoordsForAddress(address) {
+  return { lat: 40.7484445, lng: -73.9856646 }
+}
+
+module.exports = getCoordsForAddress
